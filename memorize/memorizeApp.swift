e7f2e107ce9651484e575f0d04f978d97fe3e303
Ken Hsieh <ken@innovue.ltd>
2026-03-01 15:45:39 +0800

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by dev01 on 2026/3/1.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
